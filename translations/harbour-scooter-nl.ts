1f2b9ab8cc87ea07adba3da751f57ca2a964335a
Heimen Stoffels <vistausss@fastmail.com>
2021-07-29 14:49:59 +0200

    Added Dutch translation

---
+++ translations/harbour-scooter-nl.ts
@@ -0,0 +1,455 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL" sourcelanguage="nl">
+<context>
+    <name>ActiveRideInfo</name>
+    <message>
+        <source>End ride</source>
+        <translation>Rit beëindigen</translation>
+    </message>
+    <message>
+        <source>Confirm end</source>
+        <translation>Beëindigen bevestigen</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Ride active</source>
+        <translation>Actieve rit</translation>
+    </message>
+</context>
+<context>
+    <name>Disclaimer</name>
+    <message>
+        <source>USE AT YOUR OWN RISK</source>
+        <translation>HET GEBRUIK VAN DEZE APP IS OP EIGEN RISICO</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Accepteren</translation>
+    </message>
+    <message>
+        <source>This app is provided &apos;AS-IS&apos; and with NO WARRANTIES. The app makes use of undocumented APIs of scooter providers, errors are therefore to be expected. The author of the app can not be held responsible for costs caused by the usage of the app.</source>
+        <translation>Deze app wordt ZONDER ENIGE VORM VAN GARANTIE uitgebracht. De app maakt gebruik van ongedocumenteerde api&apos;s van diensten, dus foutmeldingen zijn te verwachten. De maker van de app kan niet verantwoordelijk worden gehouden voor schade door gebruik van de app.</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <source>login</source>
+        <translation>Inloggen</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Inloggen</translation>
+    </message>
+    <message>
+        <source>Confirmation code</source>
+        <translation>Bevestigingscode</translation>
+    </message>
+    <message>
+        <source>Enter confirmation</source>
+        <translation>Voer de bevestigingscode in</translation>
+    </message>
+    <message>
+        <source>Confirm</source>
+        <translation>Bevestigen</translation>
+    </message>
+    <message>
+        <source>Enter email</source>
+        <translation>Voer je e-mailadres in</translation>
+    </message>
+    <message>
+        <source>Enter username/email</source>
+        <translation>Voer je gebruikersnaam of e-mailadres in</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>E-mailadres</translation>
+    </message>
+    <message>
+        <source>Username/email</source>
+        <translation>Gebruikersnaam/E-mailadres</translation>
+    </message>
+    <message>
+        <source>Check mail account for confirmation code</source>
+        <translation>Er is een bevestigingscode naar je Postvak IN gestuurd</translation>
+    </message>
+    <message>
+        <source>Login successful</source>
+        <translation>Het inloggen is gelukt</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>Het inloggen is mislukt</translation>
+    </message>
+</context>
+<context>
+    <name>MapPage</name>
+    <message>
+        <source>Network error</source>
+        <translation>Netwerkfout</translation>
+    </message>
+    <message>
+        <source>error</source>
+        <translation>foutmelding</translation>
+    </message>
+    <message>
+        <source>Ride</source>
+        <translation>Rit</translation>
+    </message>
+    <message>
+        <source>Ride started</source>
+        <translation>De rit is gestart</translation>
+    </message>
+    <message>
+        <source>Ride stopped</source>
+        <translation>De rit is beëindigd</translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <source>profile</source>
+        <translation>profiel</translation>
+    </message>
+    <message>
+        <source>E-Mail</source>
+        <translation>E-mailadres</translation>
+    </message>
+    <message>
+        <source>Registered</source>
+        <translation>Geregistreerd op</translation>
+    </message>
+    <message>
+        <source>Last ride</source>
+        <translation>Recentste rit</translation>
+    </message>
+    <message>
+        <source>Ride count</source>
+        <translation>Aantal ritten</translation>
+    </message>
+    <message>
+        <source>Free rides</source>
+        <translation>Vrij ritten</translation>
+    </message>
+    <message>
+        <source>Balance</source>
+        <translation>Saldo</translation>
+    </message>
+</context>
+<context>
+    <name>ProviderSettings</name>
+    <message>
+        <source>Account</source>
+        <translation>Account</translation>
+    </message>
+    <message>
+        <source>Not logged in</source>
+        <translation>Niet ingelogd</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Uitloggen</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Inloggen</translation>
+    </message>
+    <message>
+        <source>Show profile</source>
+        <translation>Profiel bekijken</translation>
+    </message>
+    <message>
+        <source>Log in to show profile</source>
+        <translation>Log in om je profiel te bekijken</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Algemeen</translation>
+    </message>
+    <message>
+        <source>Access to electric scooters provided by Bird (see https://www.bird.co). Existing account with associated payment information is needed. This app does not support creating accounts or changing payment information.</source>
+        <translation>De toegang tot elektrische scooters wordt verzorgd door Bird (https://www.bird.co). Hiervoor heb je een bestaand account met gekoppelde betaalinformatie nodig. De app heeft geen ondersteuning voor het aanmaken van accounts of koppelen van betaalinformatie.</translation>
+    </message>
+    <message>
+        <source>No description available</source>
+        <translation>Geen beschrijving beschikbaar</translation>
+    </message>
+</context>
+<context>
+    <name>RideSummary</name>
+    <message>
+        <source>Ride completed!</source>
+        <translation>De rit is beëindigd!</translation>
+    </message>
+    <message>
+        <source>Duration</source>
+        <translation>Duur</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Afstand</translation>
+    </message>
+    <message>
+        <source>Cost</source>
+        <translation>Kosten</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Sluiten</translation>
+    </message>
+    <message>
+        <source>Ride has been cancelled</source>
+        <translation>De rit is afgebroken</translation>
+    </message>
+</context>
+<context>
+    <name>ScanPage</name>
+    <message>
+        <source>Scan QR code</source>
+        <translation>QR-code scannen</translation>
+    </message>
+    <message>
+        <source>Ride</source>
+        <translation>Rit</translation>
+    </message>
+    <message>
+        <source>Code not recognized/provider not supported</source>
+        <translation>De code wordt niet herkend/Deze dienst wordt niet ondersteund</translation>
+    </message>
+</context>
+<context>
+    <name>ScooterInfo</name>
+    <message>
+        <source>Ring</source>
+        <translation>Oproepen</translation>
+    </message>
+    <message>
+        <source>Unlock</source>
+        <translation>Ontgrendelen</translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation>Niet beschikbaar</translation>
+    </message>
+    <message>
+        <source>Captive</source>
+        <translation>Captief</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>Providers</source>
+        <translation>Diensten</translation>
+    </message>
+    <message>
+        <source>Logged in</source>
+        <translation>Ingelogd</translation>
+    </message>
+    <message>
+        <source>Not logged in</source>
+        <translation>Niet ingelogd</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Algemeen</translation>
+    </message>
+    <message>
+        <source>Mapbox requires a valid account and associated API key. After generating the key, please add it in the field below and restart the app. To create an API key, consult the following article:</source>
+        <translation>Mapbox vereist een geldig account en gekoppelde api-sleutel. Genereer een sleutel, voer deze hieronder in en herstart de app. Meer informatie over het aanmaken van een api-sleutel:</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsProviderPage</name>
+    <message>
+        <source>settings</source>
+        <translation>instellingen</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Algemeen</translation>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation>Account</translation>
+    </message>
+    <message>
+        <source>Not logged in</source>
+        <translation>Niet ingelogd</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Uitloggen</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Inloggen</translation>
+    </message>
+    <message>
+        <source>Show profile</source>
+        <translation>Profiel bekijken</translation>
+    </message>
+    <message>
+        <source>Log in to show profile</source>
+        <translation>Log in om je profiel te bekijken</translation>
+    </message>
+    <message>
+        <source>Access to electric scooters provided by Bird (see https://www.bird.co). Existing account with associated payment information is needed. This app does not support creating accounts or changing payment information.</source>
+        <translation>De toegang tot elektrische scooters wordt verzorgd door Bird (https://www.bird.co). Hiervoor heb je een bestaand account met gekoppelde betaalinformatie nodig. De app heeft geen ondersteuning voor het aanmaken van accounts of koppelen van betaalinformatie.</translation>
+    </message>
+    <message>
+        <source>No description available</source>
+        <translation>Geen beschrijving beschikbaar</translation>
+    </message>
+</context>
+<context>
+    <name>UserAgreementDialog</name>
+    <message>
+        <source>login</source>
+        <translation>inloggen</translation>
+    </message>
+</context>
+<context>
+    <name>scooter::ProviderBird</name>
+    <message>
+        <source>Login failed (unexpected response)</source>
+        <translation>Het inloggen is mislukt (onverwacht antwoord)</translation>
+    </message>
+    <message>
+        <source>Login failed (no tokens)</source>
+        <translation>Het inloggen is mislukt (geen toegangssleutels)</translation>
+    </message>
+    <message>
+        <source>Login refresh failed (no tokens)</source>
+        <translation>De inlogverversing is mislukt (onverwacht antwoord)</translation>
+    </message>
+    <message>
+        <source>Login confirmation failed</source>
+        <translation>Het bevestigen is mislukt</translation>
+    </message>
+    <message>
+        <source>Login refresh failed</source>
+        <translation>De inlogverversing is mislukt</translation>
+    </message>
+    <message>
+        <source>Login request failed</source>
+        <translation>Het inlogverzoek is mislukt</translation>
+    </message>
+    <message>
+        <source>Loading profile failed</source>
+        <translation>Het profiel kan niet worden geladen</translation>
+    </message>
+    <message>
+        <source>Loading configuration failed</source>
+        <translation>De instellingen kunnen niet worden geladen</translation>
+    </message>
+    <message>
+        <source>Starting ride failed</source>
+        <translation>De rit kan niet worden gestart</translation>
+    </message>
+    <message>
+        <source>Stopping ride failed</source>
+        <translation>De rit kan niet worden beëindigd</translation>
+    </message>
+    <message>
+        <source>Scooter could not successfully be scanned / details could not be fetched</source>
+        <translation>Het voertuig kan niet worden gescand/De details kunnen niet worden opgehaald</translation>
+    </message>
+    <message>
+        <source>Problems while unlocking. Please re-check ride with official bird app.</source>
+        <translation>Het ontgrendelen is mislukt - controleer de officiële Bird-app.</translation>
+    </message>
+    <message>
+        <source>Stopping ride failed. Response did not contain expected information. Please check with official bird app, or please check no-parking zone.</source>
+        <translation>Het beëindigen is mislukt: het antwoord bevatte niet de vereiste informatie. Controleer de officiële Bird-app of kijk of er sprake is van een niet-parkerenzone.</translation>
+    </message>
+    <message>
+        <source>Stopping ride failed. Response did not contain completed stamp. Please check with official bird app, or please check no-parking zone.</source>
+        <translation>Het beëindigen is mislukt: het antwoord bevatte geen tijdstempel. Controleer de officiële Bird-app of kijk of er sprake is van een niet-parkerenzone.</translation>
+    </message>
+    <message>
+        <source>Unlocking failed. Response did not contain ride information. Please re-check ride with official bird app. Issues in response:
+</source>
+        <translation>Het ontgrendelen is mislukt: het antwoord bevatte niet de vereiste informatie. Controleer de officiële Bird-app. Foutief antwoord:
+</translation>
+    </message>
+    <message>
+        <source>Problems while stopping ride. Please re-check ride with official bird app. Issues in response:
+</source>
+        <translation>Het beëindigen is mislukt - controleer de officiële Bird-app. Foutief antwoord:
+</translation>
+    </message>
+    <message>
+        <source>Uploading photo failed</source>
+        <translation>Het uploaden is mislukt</translation>
+    </message>
+    <message>
+        <source>Problems while uploading photo. Please re-check ride with official bird app. Issues in response:
+</source>
+        <translation>De foto kan niet worden geüpload - controleer de officiële Bird-app. Foutief antwoord:
+</translation>
+    </message>
+    <message>
+        <source>Loading scooters failed</source>
+        <translation>De voertuigen kunnen niet worden geladen</translation>
+    </message>
+    <message>
+        <source>Loading areas failed</source>
+        <translation>De zones kunnen niet worden geladen</translation>
+    </message>
+    <message>
+        <source>Alarming scooter failed</source>
+        <translation>Het voertuig kan niet worden opgeroepen</translation>
+    </message>
+    <message>
+        <source>Scooter</source>
+        <translation>Voertuig</translation>
+    </message>
+    <message>
+        <source>Scooter alarm successfully triggered</source>
+        <translation>De voertuigoproep is gelukt</translation>
+    </message>
+    <message>
+        <source>Loading existing/active ride failed</source>
+        <translation>Het laden van bestaande/actieve ritten is mislukt</translation>
+    </message>
+    <message>
+        <source>Failed to load existing/active ride (missing information in response)</source>
+        <translation>Het laden van de bestaande/actieve rit is mislukt (ontbrekende informatie in antwoord)</translation>
+    </message>
+</context>
+<context>
+    <name>scooter::Scooters</name>
+    <message>
+        <source>Ride</source>
+        <translation>Rit</translation>
+    </message>
+    <message>
+        <source>Ride already active, can not start new ride</source>
+        <translation>De rit is al actief en kan niet opnieuw worden gestart</translation>
+    </message>
+    <message>
+        <source>No ride active, can not stop ride</source>
+        <translation>Er is geen rit actief en kan dus niet worden beëindigd</translation>
+    </message>
+    <message>
+        <source>Can not log out while ride is active</source>
+        <translation>Je kunt niet uitloggen zolang er nog een rit actief is</translation>
+    </message>
+    <message>
+        <source>Provider unknown/not supported</source>
+        <translation>Onbekende/Niet-ondersteunde dienst</translation>
+    </message>
+    <message>
+        <source>Provider</source>
+        <translation>Dienst</translation>
+    </message>
+</context>
+</TS>
